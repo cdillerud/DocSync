289fd49fcc2908bef48febce85bb512fc1e58210
cdillerud <cdillerud@gamerpackaging.com>
2026-09-10 15:45:29 -0500

    V117: repin controller to BOL semantic regression head

--- tools/gpi-hub-migration/Invoke-GPIHub-V117-REV2-Detached-Entry.ps1
+++ tools/gpi-hub-migration/Invoke-GPIHub-V117-REV2-Detached-Entry.ps1
@@ -15,7 +15,7 @@ $EntryPatchRepoPath = 'tools/gpi-hub-migration/v117-rev3-entry-patch.ps1frag'
 $ReplayTransformRepoPath = 'tools/gpi-hub-migration/v117-rev3-replay-transform.ps1frag'
 $ExpectedEntryPatchSha256 = '67D0F30B1A4D547C186BC15C8450C3777BF7CDA76CF49B6DCD8E01607B831537'
 $ExpectedReplayTransformSha256 = 'EDAF2B455F7F903E82E418DE38642C39E9AF79094042EDD1185797049064A7C6'
-$ExpectedFeatureCommit = '31f44e849d119b471b02866f9d7f038af53dc142'
+$ExpectedFeatureCommit = '715c24dc2e52727d5416aebb6a73c5f17c272e77'
 $EntryPatchFeatureCommit = 'dc7d1a5716b81b2a2d49d04af1e0f7d22a4e4fa1'
 
 function Require {

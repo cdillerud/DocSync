79183aafd4964b5b8ac4b9371be7c27e48e00db5
cdillerud <cdillerud@gamerpackaging.com>
2026-09-10 15:28:21 -0500

    control: repin V117 reversal anchor candidate

--- tools/gpi-hub-migration/Invoke-GPIHub-V117-REV2-Detached-Entry.ps1
+++ tools/gpi-hub-migration/Invoke-GPIHub-V117-REV2-Detached-Entry.ps1
@@ -15,7 +15,7 @@ $EntryPatchRepoPath = 'tools/gpi-hub-migration/v117-rev3-entry-patch.ps1frag'
 $ReplayTransformRepoPath = 'tools/gpi-hub-migration/v117-rev3-replay-transform.ps1frag'
 $ExpectedEntryPatchSha256 = '67D0F30B1A4D547C186BC15C8450C3777BF7CDA76CF49B6DCD8E01607B831537'
 $ExpectedReplayTransformSha256 = 'EDAF2B455F7F903E82E418DE38642C39E9AF79094042EDD1185797049064A7C6'
-$ExpectedFeatureCommit = '53cbb4335eb93dc58df1cbc2102e88211719334a'
+$ExpectedFeatureCommit = '31f44e849d119b471b02866f9d7f038af53dc142'
 $EntryPatchFeatureCommit = 'dc7d1a5716b81b2a2d49d04af1e0f7d22a4e4fa1'
 
 function Require {
